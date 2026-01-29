0521d2216e32845122bb5df55bc66d1c1c9ed899
Lucas Graf <lucasgraf01@gmail.com>
2026-01-29 15:57:27 +0100

    feat: add card stack to game board

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
