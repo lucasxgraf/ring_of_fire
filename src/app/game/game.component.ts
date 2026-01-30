8a96f2b8d6c61d9910c5c1aa796c29ccac60e332
Lucas Graf <lucasgraf01@gmail.com>
2026-01-30 13:03:07 +0100

    feat: add angular material + add btn

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -2,12 +2,18 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Game } from '../../models/game';
 import { PlayerComponent } from "../player/player.component";
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule, PlayerComponent],
+  imports: [CommonModule, 
+    PlayerComponent,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
